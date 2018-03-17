d6e5de98aaf71b2108746f61a55b56f87e5a7f5a
Edwin Alberto Triviños Ramírez <Edwin Alberto Trivios Ramrez>
2018-03-17 12:39:44 -0500

    Connecting Screens to REDUX

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { Navigation } from 'react-native-navigation';
+import { Provider } from 'react-redux';
 
 import AuthScreen from './src/screens/Auth/Auth';
 import SharePlaceScreen from './src/screens/SharePlace/SharePlace';
 import FindPlaceScreen from './src/screens/FindPlace/FindPlace';
+import configureStore from './src/store/configureStore';
+
+const store = configureStore();
 
 // Register Screens
-Navigation.registerComponent('rn-course.AuthScreen', 				() => AuthScreen);
-Navigation.registerComponent('rn-course.SharePlaceScreen', 	() => SharePlaceScreen);
-Navigation.registerComponent('rn-course.FindPlaceScreen', 	() => FindPlaceScreen);
+Navigation.registerComponent('rn-course.AuthScreen', 				() => AuthScreen, store, Provider);
+Navigation.registerComponent('rn-course.SharePlaceScreen', 	() => SharePlaceScreen, store, Provider);
+Navigation.registerComponent('rn-course.FindPlaceScreen', 	() => FindPlaceScreen, store, Provider);
 
 // Start a App
 Navigation.startSingleScreenApp({
